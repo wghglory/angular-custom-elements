8483cb02b8150b90768853b425b9812b845f5a00
Derek Wang <guanghuiw@vmware.com>
2019-12-08 21:33:14 +0800

    react uses angular custom element

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { SearchComponent } from './components/Search';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SearchComponent></SearchComponent>
       </header>
     </div>
   );
---
+++ react-app/src/components/Search.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export class SearchComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ''
+    };
+  }
+
+  setMessage(message) {
+    this.setState({ message });
+  }
+
+  closePopup() {
+    alert('closing...');
+  }
+
+  componentDidMount() {
+    this.popupEl.addEventListener('closed', this.closePopup);
+  }
+
+  componentWillUnmount() {
+    this.popupEl.removeEventListener('closed', this.closePopup);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={(e) => this.setMessage(e.target.value)}
+        />
+        <popup-element
+          ref={(ele) => (this.popupEl = ele)}
+          message={this.state.message}
+          closed={() => this.closePopup()}
+        ></popup-element>
+      </div>
+    );
+  }
+}
