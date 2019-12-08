de8beb611cea747d2fa3c53a1e0d532d090e7aad
Derek Wang <guanghuiw@vmware.com>
2019-12-08 21:43:38 +0800

    add clarity

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ClarityModule } from '@clr/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PopupComponent } from './popup/popup.component';
@@ -9,7 +11,7 @@ import { PopupService } from './popup/popup.service';
 
 @NgModule({
   declarations: [AppComponent, PopupComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule, AppRoutingModule],
   providers: [PopupService],
   bootstrap: [AppComponent],
   entryComponents: [PopupComponent]
