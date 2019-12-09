2ddd193997ce2a41d8e1664e86cdc8bd71444639
Derek Wang <guanghuiw@vmware.com>
2019-12-09 15:26:58 +0800

    tmp

--- README.md
+++ README.md
@@ -4,12 +4,20 @@
 
 ![img2](https://angular.io/generated/images/guide/elements/createElement.png)
 
-## Steps
+## Convert an Angular Component to a Custom Element
 
 1. In angular app, run `ng build --prod --output-hashing none` to build without hash in file names.
 
-2. Copy dist folder's files into react project
+1. Copy dist folder's files into react project.
+
+1. Add your component to entryComponents. This is required for any component that is defined, but not directly declared in the app.
+1. Implement the ngDoBootstrap method to manually bootstrap that app.
+1. Call createCustomElement to covert Angular's component architecture to native DOM APIs.
 
 ## References
 
 - <https://angular.io/guide/elements>
+- <https://medium.com/@balramchavan/building-simple-custom-web-element-using-angular-v6-element-667b5f02c138>
+- <https://blog.angulartraining.com/tutorial-how-to-create-custom-angular-elements-55aea29d80c5>
+- <https://medium.com/@tomsu/building-web-components-with-angular-elements-746cd2a38d5b>
+- <https://medium.com/vincent-ogloblinsky/export-angular-components-as-custom-elements-with-angular-elements-a2a0bfcd7f8a>
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -17,18 +17,22 @@ import { LoginComponent } from './login/login.component';
   declarations: [AppComponent, PopupComponent, LoginComponent],
   imports: [BrowserModule, BrowserAnimationsModule, ClarityModule, FormsModule, AppRoutingModule],
   providers: [PopupService],
-  bootstrap: [AppComponent],
-  entryComponents: [PopupComponent, LoginComponent] // to use custom elements, we have to add them here
+  // bootstrap: [AppComponent],
+  entryComponents: [PopupComponent, LoginComponent, AppComponent] // to use custom elements, we have to add them here
 })
 export class AppModule {
   constructor(private injector: Injector) {
     // Convert `PopupComponent` to a custom element.
     const PopupElement = createCustomElement(PopupComponent, { injector });
     const LoginElement = createCustomElement(LoginComponent, { injector });
+
     // Register the custom element with the browser.
     customElements.define('popup-element', PopupElement);
     customElements.define('login-element', LoginElement);
   }
 
-  ngDoBootstrap() {}
+  ngDoBootstrap() {
+    const AppElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('app-root', AppElement);
+  }
 }
